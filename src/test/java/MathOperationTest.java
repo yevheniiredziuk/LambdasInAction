3db710bf6d2eac078ac40dc02b79383075f086a6
yevheniiredziuk <yevheniiredziuk@gmail.com>
2025-08-30 18:52:40 +0300

    test addition in MathOperationTest

---
+++ src/test/java/MathOperationTest.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MathOperationTest {
+    // this test is redundant in my opinion
+    // as we don't test interface itself,
+    // only it`s implementation
+    @Test
+    void testAddition(){
+        MathOperation operation = Integer::sum;
+        assertEquals(4, operation.operate(2,2));
+        assertNotEquals(2, operation.operate(2,4));
+    }
+}

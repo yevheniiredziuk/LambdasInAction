dedabec30fcf61d34a3d0d5ed9d97f5ff1ad5aef
yevheniiredziuk <yevheniiredziuk@gmail.com>
2025-08-30 19:01:16 +0300

    test RandomNumberGeneratorTest

--- src/test/java/RandomNumberGeneratorTest.java
+++ src/test/java/RandomNumberGeneratorTest.java
@@ -1,5 +1,21 @@
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class RandomNumberGeneratorTest {
 
+    @Test
+    void testGenerateRandomNumber() {
+        int a = 10, b = 20;
+        int result = RandomNumberGenerator.generateRandomNumber(a, b);
+        assertTrue(result >= a && result <= b);
+    }
+
+    @Test
+    void whenMinEqualsMaxShouldReturnTheSameNumber() {
+        int a = 10, b = 10;
+        int result = RandomNumberGenerator.generateRandomNumber(a, b);
+        assertEquals(a, result);
+    }
 }
